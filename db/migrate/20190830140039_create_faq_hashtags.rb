2a0e285643415d6b74ff88f5a4984ddebd67be4d
Daniel Evani <danielevani97@gmail.com>
2019-10-05 22:58:36 -0300

    correcoes p2

--- db/migrate/20190830140039_create_faq_hashtags.rb
+++
@@ -1,8 +0,0 @@
-class CreateFaqHashtags < ActiveRecord::Migration[5.2]
-  def change
-    create_table :faq_hashtags do |t|
-      t.integer :faq_id
-      t.integer :hashtag_id
-    end
-  end
-end
